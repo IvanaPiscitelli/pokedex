80db21b8225d8fba29845042824405e1f9f086eb
ivana piscitelli <85pkRWi7LF>
2023-02-17 17:03:36 +0100

    get resources get external api plus rendered them dynamically plus stick footer on bottom

--- src/components/Main/Pokemon.jsx
+++ src/components/Main/Pokemon.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import style from "./Pokemon.module.css";
 
-const Pokemon = () => {
+const Pokemon = ({ id, name, img, types }) => {
   return (
     <div className={style["pokemon-card"]}>
       <div className={style["container-img-pokemon"]}>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-          alt="sprite pokemon"
-          className={style["img-pokemon"]}
-        />
+        <img src={img} alt="sprite pokemon" className={style["img-pokemon"]} />
       </div>
       <div className={style["card-body"]}>
         <div className={style["card-info"]}>
-          <h3>Name</h3>
-          <div>#01</div>
+          <h3>{name}</h3>
+          <div>#{id}</div>
         </div>
         <div className={style["card-bottom"]}>
-          <div>type</div>
+          <div className={style.types}>
+            {types.map((type) => {
+              return (
+                <div key={type.slot} className={style["type-text"]}>
+                  {type.type.name}
+                </div>
+              );
+            })}
+          </div>
           <button className={style["btn-pokemon"]}>Seen</button>
         </div>
       </div>
